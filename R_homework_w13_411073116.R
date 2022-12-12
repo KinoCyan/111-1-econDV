720a6f565ecbdcad11ab1710cbd3706a2cd5eccb
KinoCyan <87821699+KinoCyan@users.noreply.github.com>
2022-12-13 01:26:37 +0800

    Update R_homework_w13_411073116.R

--- R_homework_w13_411073116.R
+++ R_homework_w13_411073116.R
@@ -6,10 +6,12 @@ googlesheets4::read_sheet(
   sheet="sheet1"
 ) -> data2
 
-ggplot(data2, aes(x=country, y=score, fill=kinds)) +
+ggplot(data2, aes(x=country, y=scores, fill=kinds)) +
   geom_bar(stat="identity")+
-  geom_text(aes(y=score_acc, label=score), vjust=1.6, 
-            color="white", size=3.5)+
-  scale_fill_brewer(palette="Paired")+
+  ggtitle("2022世界30個國家的幸福分數細項累積","資料來源：https://worldhappiness.report/")+
+  scale_fill_brewer(palette="Accent")+
   theme_minimal()+
   coord_flip()
+
+
+  
